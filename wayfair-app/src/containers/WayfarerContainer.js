8bced63e330ca97c22ec274994db51a874b27692
candytuft <vishnyc3@gmail.com>
2021-02-23 15:09:21 -0500

    Adds initial Home Page and Container setup

---
+++ wayfair-app/src/containers/WayfarerContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WayfarerContainer extends Component {
+  render() {
+    return (
+      <h1>Wayfarer Container</h1>
+    );
+  };
+};
+
+export default WayfarerContainer;
