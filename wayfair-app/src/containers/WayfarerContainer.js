34bb8b1f7dd41e53f686b3ed5f3cc1a7d41155fc
luvycunanan <luvycunanan@gmail.com>
2021-02-23 13:48:16 -0800

    Adds fetch data

--- wayfair-app/src/containers/WayfarerContainer.js
+++ wayfair-app/src/containers/WayfarerContainer.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import CitiesList from '../components/CitiesList';
 import PostsList from '../components/PostsList'
 
+const URL = 'http://localhost:4000/api/cities'
+
+
 class WayfarerContainer extends Component {
+  state = {
+    cities: []
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        cities: json
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div>
