b755187a6d6ab43e0f9f2dfcef888ff75aec3db1
abewallah <ibrahimabdull93@gmail.com>
2021-02-24 15:21:40 -0800

    styling

--- wayfair-app/src/components/PostCard.js
+++ wayfair-app/src/components/PostCard.js
@@ -7,8 +7,8 @@ function PostCard(props) {
 
   return (
     <div>
-      <h1>{props.post.title}</h1>
-      <p>{props.post.body}</p>
+      <Link to={`/show-post/${props.post._id}`}>{props.post.title}</Link>
+      {/* <p>{props.post.body}</p> */}
       <Link to={`/edit-post/${props.post._id}`}><button className='btn-primary'>Edit</button></Link>
       <button className='btn-primary' onClick={() => props.deletePost(props.post._id)}>Delete</button>
     </div>
--- wayfair-app/src/pages/CityPage.js
+++ wayfair-app/src/pages/CityPage.js
@@ -1,16 +1,26 @@
-import PostLists from '../components/PostsList'
+import PostLists from '../components/PostsList';
 import CreatePostPage from '../pages/CreatePostPage';
 
 const CityPage = (props) => {
-  return(
+  return (
     <div>
-      <h1>{props.city ? props.city.city : ""}</h1>
-      {props.city ? <PostLists deletePost={props.deletePost} city={props.city}/> : ""}
-
+      <div className='card'>
+        <img className='card-img-top' src={props.city ? props.city.img : ''} />
+        <div className='card-body'>
+          <h1 className='card-title'>{props.city ? props.city.city : ''}</h1>
+        </div>
+        <div className='card'>
+          {props.city ? (
+            <PostLists deletePost={props.deletePost} city={props.city} />
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
       {/* <button onClick={CreatePostPage}><h1>+</h1></button> */}
       <CreatePostPage city={props.city} />
     </div>
   );
-}
+};
 
 export default CityPage;
