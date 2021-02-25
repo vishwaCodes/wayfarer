45872f648e0d2d2778ac693969be8460b1bb052b
luvycunanan <luvycunanan@gmail.com>
2021-02-25 15:53:37 -0800

    Adjusts post css

--- wayfair-app/src/components/PostCard.js
+++ wayfair-app/src/components/PostCard.js
@@ -12,35 +12,37 @@ function PostCard(props) {
 
   return (
     <div>
-      <div classNam='card'>
-        <div className='row'>
-          <div className='col-5'>
-            <Link to={`/show-post/${props.post._id}`}>{props.post.title}</Link>
+      <div classNam='card post-card'>
+        <div className='row' id='post-div' >
+          <div className="post-div" >
+            <Link className="post-link" to={`/show-post/${props.post._id}`}>{props.post.title}</Link>
           </div>
-          <div className='col-7'>
-            <Link to={`/edit-post/${props.post._id}`}><button className='btn btn-warning'>Edit</button>{' '}</Link>
-          <div className={`modalBackground modalShowing-${modalState}`}>
-          <div className="modalInner">
-          <div className="wrapper fadeInDown">
-              <div id="formContent">
-                <div className="modal-header flex-column">
-                  <div className="icon-box">
-                    <img id="deleteIcon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9xuqoEn6bxVzH0Jlu09DgEEeKihPLE0CsaQ&usqp=CAU" alt=""/>
+          <div className='btn-div'>
+          <Link to={`/edit-post/${props.post._id}`}><button className='btn btn-warning post-btns'>Edit</button>{' '}</Link>
+                  <button className='btn btn-warning post-btns' onClick={() => toggleModalState()} >Delete</button>
+          </div>
+          <div>
+            <div className={`modalBackground modalShowing-${modalState}`}>
+              <div className="modalInner">
+                <div className="wrapper fadeInDown">
+                  <div id="formContent">
+                    <div className="modal-header flex-column">
+                      <div className="icon-box">
+                        <img id="deleteIcon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9xuqoEn6bxVzH0Jlu09DgEEeKihPLE0CsaQ&usqp=CAU" alt=""/>
+                      </div>
+                    </div>
+                      <h5 className="modal-title w-100">Please confirm</h5>
+                    <div className="modal-body">
+                      <p>Are you sure you want to delete this post?</p>
+                    </div>  
+                    <div className="justify-content-center">
+                      <button id="cancelBtn" className="btn btn-secondary" onClick={() => toggleModalState()}>Cancel</button>
+                      <button id="deletePostBtn" className="btn btn-danger" onClick={() => props.deletePost(props.post._id)}>Delete</button>
+                    </div>
                   </div>
                 </div>
-                  <h5 className="modal-title w-100">Please confirm</h5>
-                <div className="modal-body">
-                  <p>Are you sure you want to delete this post?</p>
-                </div>  
-                <div className="justify-content-center">
-                <button id="cancelBtn" className="btn btn-secondary" onClick={() => toggleModalState()}>Cancel</button>
-                <button id="deletePostBtn" className="btn btn-danger" onClick={() => props.deletePost(props.post._id)}>Delete</button>
-                </div>
               </div>
-          </div>
-        </div>
-          </div>
-      <button className='btn btn-warning' onClick={() => toggleModalState()} >Delete</button> 
+            </div>
           </div>
         </div>
       </div>
