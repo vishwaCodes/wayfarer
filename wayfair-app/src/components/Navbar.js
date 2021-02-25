c53f3de1f76cac9ad8ff7ce46dca2fa0fa4fa755
candytuft <vishnyc3@gmail.com>
2021-02-25 11:49:12 -0500

    Implements Login Modal

--- wayfair-app/src/components/Navbar.js
+++ wayfair-app/src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Navbar = () => {
+
+  const [modalState, setModalState] = useState(false);
+
+  const toggleModalState = () => {
+    setModalState(!modalState)
+  }
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
@@ -19,8 +25,30 @@ const Navbar = () => {
             <a className="nav-link" href="/signup">Sign Up</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/login">Log In</a>
+            <a onClick={() => toggleModalState()} className="nav-link">Log In</a>
           </li>
+          <div className={`modalBackground modalShowing-${modalState}`}>
+        <div className="modalInner">
+          <div className="wrapper fadeInDown">
+              <div id="formContent">
+                <div className="fadeIn first">
+                  <img src="https://img.icons8.com/bubbles/2x/login-rounded-right.png" id="icon" alt="User Icon"/>
+                </div>
+                <form action="">
+                  <input type="email" id="login" className="fadeIn second" name="login" placeholder="Email" />
+                  <input type="text" id="password" className="fadeIn third" name="login" placeholder="Password"></input>
+                  <input id="loginButton" type="submit" class="fadeIn fourth" value="Log In"></input>
+                </form>
+                <div id="formFooter">
+                  <a href="/" className="underlineHover">Forgot Password?</a>
+                </div>
+                <button id="exitBtn"  onClick={() => toggleModalState()}>
+                  Exit
+                </button>
+              </div>
+          </div>
+        </div>
+      </div>
           <li className="nav-item">
             <a className="nav-link" href="/create">Create a Post</a>
           </li>
