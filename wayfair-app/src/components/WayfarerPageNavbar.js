988770f683f4c0270c12fc1544d25f07025064bb
luvycunanan <luvycunanan@gmail.com>
2021-02-24 16:37:58 -0800

    Reverses order to new post first

---
+++ wayfair-app/src/components/WayfarerPageNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div className="container">
+      <a className="navbar-brand" href="/wayfarer">Wayfarer</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+            <a className="nav-link" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/signup">Sign Up</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/login">Log In</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  );
+};
+
+export default Navbar;
