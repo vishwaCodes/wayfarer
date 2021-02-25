ab64834d095515381339fee3312e953cdb26e682
candytuft <vishnyc3@gmail.com>
2021-02-24 19:16:25 -0500

    Starts adding delete modal

--- wayfair-app/src/components/PostCard.js
+++ wayfair-app/src/components/PostCard.js
@@ -1,17 +1,24 @@
-import {Link } from 'react-router-dom'
+import {Link } from 'react-router-dom';
+import React, {useState} from 'react';
+
 
 
 function PostCard(props) {
+  const [modalState, setModalState] = useState(false);
 
-
+  const toggleModalState = () => {
+    setModalState(!modalState)
+  }
 
   return (
-    <div>
+    <div className={`modalBackground modalShowing-${modalState}`}>
       <Link to={`/show-post/${props.post._id}`}>{props.post.title}</Link>
       {/* <p>{props.post.body}</p> */}
       <Link to={`/edit-post/${props.post._id}`}><button className='btn-primary'>Edit</button></Link>
-      <button className='btn-primary' onClick={() => props.deletePost(props.post._id)}>Delete</button>
+      
+      <button className='btn-primary' onClick={() => toggleModalState()}>Delete</button>
     </div>
+    
   );
 };
 
